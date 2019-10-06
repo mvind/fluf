445770654fa0ef2a36d5bb20aff6692a19123799
mvind <vms754@alumni.ku.dk>
2019-10-06 21:55:21 +0200

    first commit

---
+++ r_backend/main.R
@@ -0,0 +1,19 @@
+# > version
+# _                           
+# platform       x86_64-w64-mingw32          
+# arch           x86_64                      
+# os             mingw32                     
+# system         x86_64, mingw32             
+# status                                     
+# major          3                           
+# minor          5.2                         
+# year           2018                        
+# month          12                          
+# day            20                          
+# svn rev        75870                       
+# language       R                           
+# version.string R version 3.5.2 (2018-12-20)
+# nickname       Eggshell Igloo     
+
+
+# First commit
