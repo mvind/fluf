54249944b19093b30c7a47fe0a68faa6be174f78
mvind <vms754@alumni.ku.dk>
2019-10-08 21:14:51 +0200

    Basic testing of getting forex data

--- r_backend/main.R
+++ r_backend/main.R
@@ -15,5 +15,46 @@
 # version.string R version 3.5.2 (2018-12-20)
 # nickname       Eggshell Igloo     
 
+if(!"quantmod" %in% installed.packages()[, 1]) {
+  install.packages("quantmod")
+}
 
-# First commit
+library("quantmod")
+
+# So far quantmod seems like best choice 
+# return object: 
+#   ticker ??
+#   time 
+#   forex data (which specific kind is it?)
+#   NA's???
+
+# forex data --------------------------------------------------------------
+
+forexGetter <- function(TICKER = "USD/JPY", TO = Sys.Date(), 
+                        FROM = "2005-06-02") {
+  # TO, FROM: CCYY - MM - DD 
+  # TICKER: CURRENCY/CROSS  
+  time_diff <- as.numeric(invisible(difftime(TO, FROM, units = "days")))
+  n_months <- time_diff %/% 179
+  
+  data_out <- new.env()
+  quantmod::getFX(TICKER, FROM, TO, env = data_out)
+  return(data_out)
+}
+
+library(xts)
+xts::to
+tick1 <- "USD/JPY"
+out <- forexGetter(tick1)
+Sys.Date() - 179 * (1:3)
+a <- invisible(difftime(Sys.Date(), "2005-06-02", units = "days"))
+# gdp data ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
