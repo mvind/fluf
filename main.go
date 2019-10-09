aa7323511b49b019bf560480c86855f5652f43c0
mvind <vms754@alumni.ku.dk>
2019-10-09 21:30:00 +0200

    ...

--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import(
   "fmt"
+  "github.com/senseyeio/roger"
+
 )
 
+// We need to create to dynamicly start a R enviroment to where we can run
+// run.Rserve() using the Rserve package ???
+// And then we run this file
+
 func main() {
-    fmt.Println("Hello World")
+  rClient, err := roger.NewRClient("127.0.0.1", 6311)
+  if err != nil {
+    fmt.Println("Failed to connect")
+    return
+  }
+
+  value, err := rClient.Eval("pi")
+  if err != nil {
+    fmt.Println("Command failed: " + err.Error())
+  } else {
+    fmt.Println(value) // 3.141592653589793
+  }
+
+  helloWorld, _ := rClient.Eval("as.character('Hello World')")
+  fmt.Println(helloWorld) // Hello World
+
+  arrChan := rClient.Evaluate("Sys.sleep(5); c(1,1)")
+  arrResponse := <-arrChan
+  arr, _ := arrResponse.GetResultObject()
+  fmt.Println(arr) // [1, 1]
 }
