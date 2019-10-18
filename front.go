fbcf088cbf1c072346d624be1f017288f613d35c
mvind <vms754@protonmail.com>
2019-10-18 06:38:41 +0200

    First working container

--- front.go
+++ front.go
@@ -1,50 +1,72 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/mum4k/termdash"
 	"github.com/mum4k/termdash/container"
-	"github.com/mum4k/termdash/linestyle"
 	"github.com/mum4k/termdash/terminal/termbox"
-	"github.com/mum4k/termdash/terminal/terminalapi"
-	"github.com/mum4k/termdash/widgets/barchart"
 )
+
+// func main() {
+// 	t, err := termbox.New()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer t.Close()
+
+// 	bc, err := barchart.New()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	values := []int{1, 2, 3, 4, 5}
+// 	max := 10
+// 	if err := bc.Values(values, max); err != nil {
+// 		panic(err)
+// 	}
+
+// 	c, err := container.New(
+// 		t,
+// 		container.Border(linestyle.Light),
+// 		container.BorderTitle("PRESS Q TO QUIT"),
+// 		container.PlaceWidget(bc),
+// 	)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	quitter := func(k *terminalapi.Keyboard) {
+// 		if k.Key == 'q' || k.Key == 'Q' {
+// 			cancel()
+// 		}
+// 	}
+
+// 	if err := termdash.Run(ctx, t, c, termdash.KeyboardSubscriber(quitter)); err != nil {
+// 		panic(err)
+// 	}
+
+// }
 
 func main() {
 	t, err := termbox.New()
 	if err != nil {
-		panic(err)
+		fmt.Errorf("termbox.New => %v", err)
 	}
+
 	defer t.Close()
 
-	bc, err := barchart.New()
+	c, err := container.New(t)
 	if err != nil {
-		panic(err)
+		fmt.Errorf("termbox.New => %v", err)
 	}
 
-	values := []int{1, 2, 3, 4, 5}
-	max := 10
-	if err := bc.Values(values, max); err != nil {
-		panic(err)
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	c, err := container.New(
-		t,
-		container.Border(linestyle.Light),
-		container.BorderTitle("PRESS Q TO QUIT"),
-		container.PlaceWidget(bc),
-	)
-	if err != nil {
-		panic(err)
+	if err := termdash.Run(ctx, t, c); err != nil {
+		fmt.Errorf("termbox.New => %v", err)
 	}
-
-	quitter := func(k *terminalapi.Keyboard) {
-		if k.Key == 'q' || k.Key == 'Q' {
-			cancel()
-		}
-	}
-
-	if err := termdash.Run(ctx, t, c, termdash.KeyboardSubscriber(quitter)); err != nil {
-		panic(err)
-	}
-
 }
