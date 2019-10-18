14c6ac218b318ba51af4ef4c6031e8bb150d9c85
mvind <vms754@protonmail.com>
2019-10-18 15:41:01 +0200

    Added clock/date widget

--- front.go
+++ front.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/mum4k/termdash/widgets/text"
 
 	"github.com/mum4k/termdash/linestyle"
 
@@ -18,8 +21,45 @@ import (
 // i.e when we input characters in the search and so on..
 
 // Create header (Search bar, date and time, session info)
+// How to add widgets??
+// Make make custom colors
 
-const reDraw = 10 // Milliseconds until we refresh screen
+// Create segment display
+// call go func(ctx, obj) and then its works??
+
+func clock(ctx context.Context, t *text.Text) {
+	ticker := time.NewTicker(time.Second) // Channel to time every second
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			now := time.Now() // New tick
+			nowStr := now.Format("15 04")
+			parts := strings.Split(nowStr, " ")
+
+			spacer := ":"
+			if now.Second()%2 == 0 {
+				spacer = " "
+			}
+
+			date := now.Format("Jan 2 Mon")
+
+			//fmt.Println(date)
+
+			t.Reset() // First delete text buffer
+
+			if err := t.Write(parts[0] + spacer + parts[1] + " - " + date); err != nil {
+				panic(err)
+			}
+
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+const reDraw = 250 * time.Millisecond
 
 func main() {
 	t, err := termbox.New()
@@ -28,6 +68,16 @@ func main() {
 	}
 	defer t.Close()
 
+	ctx, cancel := context.WithCancel(context.Background())
+
+	clockCon, err := text.New()
+	if err != nil {
+		panic(err)
+	}
+	//clockCon.Write("hej")
+	go clock(ctx, clockCon)
+
+	// Layout
 	c, err := container.New(
 		t,
 		container.SplitHorizontal(
@@ -38,6 +88,7 @@ func main() {
 					),
 					container.Right( // time and date
 						container.Border(linestyle.Light),
+						container.PlaceWidget(clockCon),
 					),
 					container.SplitPercent(80),
 				),
@@ -52,19 +103,13 @@ func main() {
 		fmt.Errorf("termbox.New => %v", err)
 	}
 
-	// ctx, cancel := context.WithTimeout(context.Background(), reDraw*time.Millisecond)
-	// defer cancel()
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	quitter := func(k *terminalapi.Keyboard) {
 		if k.Key == 'q' || k.Key == 'Q' {
 			cancel()
 		}
 	}
 
-	if err := termdash.Run(ctx, t, c, termdash.RedrawInterval(reDraw*time.Millisecond), termdash.KeyboardSubscriber(quitter)); err != nil {
+	if err := termdash.Run(ctx, t, c, termdash.RedrawInterval(reDraw), termdash.KeyboardSubscriber(quitter)); err != nil {
 		fmt.Errorf("termbox.New => %v", err)
 	}
 }
