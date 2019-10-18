8f924f28538588abd56aa771fe53ee7b71be5409
mvind <vms754@protonmail.com>
2019-10-18 07:51:25 +0200

    Basic layout binary tree finished

--- front.go
+++ front.go
@@ -5,68 +5,66 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/mum4k/termdash/linestyle"
+
+	"github.com/mum4k/termdash/terminal/terminalapi"
+
 	"github.com/mum4k/termdash"
 	"github.com/mum4k/termdash/container"
 	"github.com/mum4k/termdash/terminal/termbox"
 )
 
-// func main() {
-// 	t, err := termbox.New()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer t.Close()
+// We only need to redraw the screen when input changes happens right?
+// i.e when we input characters in the search and so on..
 
-// 	bc, err := barchart.New()
-// 	if err != nil {
-// 		panic(err)
-// 	}
+// Create header (Search bar, date and time, session info)
 
-// 	values := []int{1, 2, 3, 4, 5}
-// 	max := 10
-// 	if err := bc.Values(values, max); err != nil {
-// 		panic(err)
-// 	}
-
-// 	c, err := container.New(
-// 		t,
-// 		container.Border(linestyle.Light),
-// 		container.BorderTitle("PRESS Q TO QUIT"),
-// 		container.PlaceWidget(bc),
-// 	)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	quitter := func(k *terminalapi.Keyboard) {
-// 		if k.Key == 'q' || k.Key == 'Q' {
-// 			cancel()
-// 		}
-// 	}
-
-// 	if err := termdash.Run(ctx, t, c, termdash.KeyboardSubscriber(quitter)); err != nil {
-// 		panic(err)
-// 	}
-
-// }
+const reDraw = 10 // Milliseconds until we refresh screen
 
 func main() {
 	t, err := termbox.New()
 	if err != nil {
 		fmt.Errorf("termbox.New => %v", err)
 	}
-
 	defer t.Close()
 
-	c, err := container.New(t)
+	c, err := container.New(
+		t,
+		container.SplitHorizontal(
+			container.Top(
+				container.SplitVertical(
+					container.Left( // search
+						container.Border(linestyle.Light),
+					),
+					container.Right( // time and date
+						container.Border(linestyle.Light),
+					),
+					container.SplitPercent(80),
+				),
+			),
+			container.Bottom(
+				container.Border(linestyle.Light),
+			),
+			container.SplitPercent(10),
+		),
+	)
 	if err != nil {
 		fmt.Errorf("termbox.New => %v", err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// ctx, cancel := context.WithTimeout(context.Background(), reDraw*time.Millisecond)
+	// defer cancel()
+
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	if err := termdash.Run(ctx, t, c); err != nil {
+	quitter := func(k *terminalapi.Keyboard) {
+		if k.Key == 'q' || k.Key == 'Q' {
+			cancel()
+		}
+	}
+
+	if err := termdash.Run(ctx, t, c, termdash.RedrawInterval(reDraw*time.Millisecond), termdash.KeyboardSubscriber(quitter)); err != nil {
 		fmt.Errorf("termbox.New => %v", err)
 	}
 }
