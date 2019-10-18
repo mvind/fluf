8a76676cfab5b37381a8d1a703b4d6703f334f3a
mvind <vms754@protonmail.com>
2019-10-18 19:19:27 +0200

    Work on exec Rserve setup

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # fluf
 This is a hobby project im doing in late hours / early mornings when people normaly sleep.
 
+## Future ideas 
+- Search suggestions
+- color themes?
+- Date search intervals
+- Certain search syntax, i.e FX JPY USD, and if we cant detect syntax make text blink red when pressing enter
+
 ## TODO
 - [x] Write a todo list (6/9/19)
 - [x] MINIMAL VIABLE PRODUCT added (8/9/19)
   - **DATA/BACKEND**
-  - [ ] forex data
+  - [x] forex data
   - [ ] gdp data
   - **GO GUI**
   - [ ] Figure out a way to run R in GO dynamicly (9/9/19)
   - [ ] dynamic search field
   - [ ] linechart
   - [ ] datatable
-- [ ] Find R packages for finance data, look at geo-fred (7/9/19) not done
+- [x] Find R packages for finance data, look at geo-fred (7/9/19) not done
 - [ ] Find out optimal data formatting for r data retrivival to golang front end
 - [ ] Find golang package which can compile r scripts
 - [ ] **BUILD GO PACKAGE TO GET FINANCE DATA** (I.e setup version control and stuf like that)
--- main.go
+++ main.go
@@ -2,37 +2,47 @@ package main
 
 import (
 	"fmt"
-	// "path/filepath"
-	"github.com/senseyeio/roger"
+	"os/exec"
+	//	"github.com/senseyeio/roger"
 )
 
-// We need to create to dynamicly start a R enviroment to where we can run
-// run.Rserve() using the Rserve package ???
-// And then we run this file
-
-// Learn about type structures in golang
-// See the returned object
+/*
+TODO / IDEAS:
+We need a way to install Rserve if its not currently installed
+Get pid number to kill daemon thread
+1 socket pr instance, note eval function fucks with Rserve with async eval calls
+*/
 
 func main() {
-	// p := "./p/p"
-	// q := ".\\q\\q"
-	//
-	// fmt.Println(path.base("C:\\Users\\morte_000\\github\\"))
-	// WIN package root
-	// win_path := "C:\Users\morte_000\github\fluf\r_backend"
-	//
-	// fmt.Println(win_path)
-	rClient, err := roger.NewRClient("127.0.0.1", 6311)
+	//var query string
+	//query = "FX USD JPN" // test
+
+	// Setup Rserve
+	//r := "R CMD Rserve"
+	//pid := []string{"ax", "|", "grep", "Rserve"}
+
+	//cmd := exec.Command("/bin/ps", "ax", "|")
+	cmd := exec.Command("/tmp/Rserv", "R CMD Rserve")
+	out, err := cmd.Output()
 	if err != nil {
-		fmt.Println("Failed to connect")
-		return
+		panic(err)
 	}
 
-	value, err := rClient.Eval("source(\"C:/Users/morte_000/github/fluf/r_backend/test123.R\")")
-	if err != nil {
-		fmt.Printf("Command failed: " + err.Error())
-	}
-	// git setup
-	n := len(value)
-	fmt.Println(n)
+	fmt.Println(string(out))
+
+	// fmt.Println("GETTING: " + query)
+	// rClient, err := roger.NewRClient("127.0.0.1", 6311)
+	// if err != nil {
+	// 	fmt.Println("Failed to connect")
+	// 	return
+	// }
+
+	// value, err := rClient.Eval("source(\"C:/Users/morte_000/github/fluf/r_backend/test123.R\")")
+	// if err != nil {
+	// 	fmt.Printf("Command failed: " + err.Error())
+	// }
+	// // git setup
+	// //n := len(value)
+	// fmt.Println(value)
+
 }
